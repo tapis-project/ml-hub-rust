223f4c64ec527d8d4bfdd9af19000546d482bca3
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-02-13 11:47:19 -0600

    Link nfs server to models and datasets deployments

--- src/apis/datasets/scripts/burnup.sh
+++ src/apis/datasets/scripts/burnup.sh
@@ -3,6 +3,11 @@
 set -e
 
 rootDir=$1
+nfsServerIp=$2
+nfsServerIpTemplate="{{ NFS_SERVER_COMPONENT_IP }}"
+
+sed -i.bak "s|${nfsServerIpTemplate}|${nfsServerIp}|g" "$rootDir/deploy/local/minikube/deployment.yaml"
+rm "$rootDir/deploy/local/minikube/deployment.yaml.bak"
 
 kubectl apply -f "$rootDir/deploy/local/minikube/service.yaml" \
     -f "$rootDir/deploy/local/minikube/deployment.yaml"
