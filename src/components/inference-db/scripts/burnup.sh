e54f86c911a5e1f1ce96fce25b5f2cfb8175fa8a
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-06-19 14:06:43 -0500

    Add inference db

---
+++ src/components/inference-db/scripts/burnup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+rootDir=$1
+
+kubectl apply -f "$rootDir/deploy/local/minikube/service.yaml" \
+    -f "$rootDir/deploy/local/minikube/pvc.yaml" \
+    -f "$rootDir/deploy/local/minikube/cm-init-inference-db-script.yaml" \
+    -f "$rootDir/deploy/local/minikube/deployment.yaml" \
+    
