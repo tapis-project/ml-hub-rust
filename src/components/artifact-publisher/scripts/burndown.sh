383aeb6a5b1250fee0b31f1d6ca61515e0d3feb1
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-08-20 12:07:59 -0500

    Update gh actions and up/down scripts

--- src/components/artifact-publisher/scripts/burndown.sh
+++ src/components/artifact-publisher/scripts/burndown.sh
@@ -3,10 +3,5 @@
 set -e
 
 rootDir=$1
-nfsServerIp=$2
-nfsServerIpTemplate="{{ NFS_SERVER_COMPONENT_IP }}"
-
-sed -i.bak "s|${nfsServerIp}|${nfsServerIpTemplate}|g" "$rootDir/deploy/local/minikube/deployment.yaml"
-rm "$rootDir/deploy/local/minikube/deployment.yaml.bak"
 
 kubectl delete -f "$rootDir/deploy/local/minikube/deployment.yaml"
--- src/components/artifact-publisher/scripts/burnup.sh
+++ src/components/artifact-publisher/scripts/burnup.sh
@@ -6,7 +6,12 @@ rootDir=$1
 nfsServerIp=$2
 nfsServerIpTemplate="{{ NFS_SERVER_COMPONENT_IP }}"
 
+# Replace the template with the nfs server ip
 sed -i.bak "s|${nfsServerIpTemplate}|${nfsServerIp}|g" "$rootDir/deploy/local/minikube/deployment.yaml"
 rm "$rootDir/deploy/local/minikube/deployment.yaml.bak"
 
 kubectl apply -f "$rootDir/deploy/local/minikube/deployment.yaml"
+
+# Return the manifest back to the template
+sed -i.bak "s|${nfsServerIp}|${nfsServerIpTemplate}|g" "$rootDir/deploy/local/minikube/deployment.yaml"
+rm "$rootDir/deploy/local/minikube/deployment.yaml.bak"
