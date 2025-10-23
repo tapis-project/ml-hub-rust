ede823a2471309f50fcd3072e1235468ca3be408
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-10-23 10:41:16 -0500

    move skills and domains back out to top level entities

--- src/libs/shared/src/domain/entities/mod.rs
+++ src/libs/shared/src/domain/entities/mod.rs
@@ -1,4 +1,5 @@
-pub mod agents;
+pub mod domains;
+pub mod skills;
 pub mod artifact;
 pub mod artifact_ingestion;
 pub mod timestamp;
--- src/libs/shared/src/domain/entities/skills/agntcy_skill_to_skill.rs
+++ src/libs/shared/src/domain/entities/skills/agntcy_skill_to_skill.rs
@@ -1,5 +1,5 @@
 use crate::domain::external::agntcy::skill as agntcy;
-use crate::domain::entities::agents::skills as entities;
+use crate::domain::entities::skills as entities;
 
 impl From<agntcy::Skill> for entities::Skill {
     fn from(value: agntcy::Skill) -> Self {
