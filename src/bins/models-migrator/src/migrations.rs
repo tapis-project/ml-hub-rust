2a76f68e2dc64f7a5533ad4facab4907b25d5e73
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-11-25 10:25:05 -0600

    Create index on task_types field for MODEL_METADATA collection

--- src/bins/models-migrator/src/migrations.rs
+++ src/bins/models-migrator/src/migrations.rs
@@ -7,6 +7,7 @@ use shared::infra::persistence::mongo::documents::model_metadata::ModelMetadata;
 pub fn get_migrations() -> Vec<Box<dyn Migration>> {
     vec![
         Box::new(CreateModelAuthorNameIndex),
+        Box::new(CreateTaskTypesIndex),
     ]
 }
 
@@ -35,6 +36,34 @@ impl Migration for CreateModelAuthorNameIndex {
     async fn down(&self, env: Env) -> anyhow::Result<()> {
         let db: &Database = &env.db.unwrap();
         db.collection::<ModelMetadata>(MODEL_METADATA_COLLECTION).drop_index("create_model_author_name_index_unique").await?;
+        Ok(())
+    }
+}
+
+pub struct CreateTaskTypesIndex;
+
+#[async_trait]
+impl Migration for CreateTaskTypesIndex {
+    async fn up(&self, env: Env) -> anyhow::Result<()> {
+        let db: &Database = &env.db.unwrap();
+        db.collection::<ModelMetadata>(MODEL_METADATA_COLLECTION)
+            .create_index(
+                mongodb::IndexModel::builder()
+                    .keys(doc! { "task_types": 1 })
+                    .options(
+                        Some(mongodb::options::IndexOptions::builder()
+                            .name("create_task_types_index".to_string())
+                            .build())
+                    )
+                    .build()
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, env: Env) -> anyhow::Result<()> {
+        let db: &Database = &env.db.unwrap();
+        db.collection::<ModelMetadata>(MODEL_METADATA_COLLECTION).drop_index("create_task_types_index").await?;
         Ok(())
     }
 }
