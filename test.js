cf4e3adf27e8a0fae53c6251c5c64ee397731f20
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-12-15 16:45:42 -0600

    debug for mongo query and trigger ci build

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
-db.MODEL_METADATA.aggregate([
-    { $match: { $or: [
-      {task_types: ["TextGeneration"] }
-    ] }},
+const cur = db.MODEL_METADATA.aggregate([
+    { 
+      $match: {
+        $or: [
+          { task_types: ["TextGeneration"] }
+        ]
+      }
+    },
     { $sort: { _id: 1 } },
     { $limit: 20 },
-  ])
+  ], {
+    maxTimeMS: 2000,
+    allowDiskUse: true,
+    cursor: { batchSize: 100 },
+    comment: "Model Discovery Search"
+  })
+
+  while (cur.hasNext()) {
+    cur.next()
+  }
