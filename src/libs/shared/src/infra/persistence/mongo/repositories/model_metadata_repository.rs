ad9966b9fcf91b37fbdd8fa88863c3034412a7ac
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-12-15 15:30:56 -0600

    Fix model discovery query

--- src/libs/shared/src/infra/persistence/mongo/repositories/model_metadata_repository.rs
+++ src/libs/shared/src/infra/persistence/mongo/repositories/model_metadata_repository.rs
@@ -5,6 +5,7 @@ use crate::application::outputs::discover_models::DiscoverModelsOutput;
 use crate::{application, domain};
 use crate::domain::entities;
 use bson::oid::ObjectId;
+use bson::to_document;
 use mongodb::{
     bson::{
         doc,
@@ -20,7 +21,6 @@ use mongodb::{
 };
 use async_trait::async_trait;
 use futures::stream::TryStreamExt;
-use serde::Deserialize;
 
 use super::super::database::MODEL_METADATA_COLLECTION;
 use super::super::documents::model_metadata_filter::ModelMetadataFilter;
@@ -29,17 +29,6 @@ use super::super::documents::model_metadata::ModelMetadata;
 pub struct ModelMetadataRepository {
     read_collection: Collection<ModelMetadata>,
     write_collection: Collection<ModelMetadata>
-}
-
-#[derive(Deserialize)]
-pub struct Count {
-    total: i64
-}
-
-#[derive(Deserialize)]
-pub struct SearchResponseDocument {
-    results: Vec<ModelMetadata>,
-    count: Option<Vec<Count>>
 }
 
 impl ModelMetadataRepository {
@@ -119,56 +108,40 @@ impl application::ports::repositories::ModelMetadataRepository for ModelMetadata
             filters.push(serialized_metadata);
         }
 
+        let mut aggregate: Vec<Document> = vec![];
+
         // Return documents the meet the criteria and sort by _id
-        let mut results_pipeline = vec![
-            doc! {
-                "$match": { "$or": filters },
-            }
-        ];
-        
-        // Sort by _id by default
-        results_pipeline.push(
-            doc! {
-                "$sort": { "_id": 1 }
-            }
-        );
-        
+        let mut match_document_value = doc! { "$or": filters };
+
         // Find all documents after the cursor
         if let Some(pagination_cursor) = input.options.cursor() {
             let oid = ObjectId::parse_str(pagination_cursor)
                 .map_err(|err| ApplicationError::RepoError(format!("Invalid cursor value: {}", err.to_string())))?;
 
-            results_pipeline.push(
-                doc! {
-                    "$match": {
-                        "_id": { "$gt": oid }
-                    }
-                }
-            )
+            match_document_value.extend(doc! { "_id": { "$gt": oid }});
         }
+
+        let match_document = doc! {
+            "$match": match_document_value,
+        };
+
+        aggregate.push(match_document);
+        
+        // Sort by _id by default
+        aggregate.push(
+            doc! {
+                "$sort": { "_id": 1 }
+            }
+        );
 
         // Return a limited number of docuemtns
         if let Some(limit) = input.options.limit() {
-            results_pipeline.push(
+            aggregate.push(
                 doc! {
                     "$limit": limit as i64
                 }
             )
         }
-        
-        // Add results pipeline to the facet statemnt
-        let mut facet_doc = Document::new();
-        facet_doc.insert("results", results_pipeline);
-
-        // Return the count if requested
-        if input.options.include_count().unwrap_or_else(|| false) {
-            facet_doc.insert("count", vec![doc! { "$count": "total" }]);
-        }
-
-        // Construct the facet document
-        let pipeline = vec![
-            doc! { "$facet": facet_doc }
-        ];
 
         // Aggregate options
         let options = AggregateOptions::builder()
@@ -178,32 +151,34 @@ impl application::ports::repositories::ModelMetadataRepository for ModelMetadata
             .max_time(Some(Duration::from_secs(2)))
             .build();
 
-        let mut cursor = self.read_collection.aggregate(pipeline, Some(options))
+        let mut cursor = self.read_collection.aggregate(aggregate, Some(options))
             .await
             .map_err(|err| ApplicationError::RepoError(err.to_string()))?;
 
-
         let mut models: Vec<entities::model_metadata::ModelMetadata> = Vec::new();
-        let mut count: Option<i64> = None;
         let mut pagination_cursor: Option<String> = None;
-        if let Some(doc) = cursor.try_next().await.map_err(|err| ApplicationError::RepoError(err.to_string()))?  {
-            let response: SearchResponseDocument = from_document(doc)
-                .map_err(|err| ApplicationError::RepoError(format!("Failed to convert to SearchResponseDocument: {}", err.to_string())))?;
+        let mut last_id: Option<ObjectId> = None;
+        while let Some(entry) = cursor.try_next().await.map_err(|err| ApplicationError::RepoError(err.to_string()))?  {
+            let doc: ModelMetadata = from_document(entry)
+                .map_err(|err| ApplicationError::RepoError(format!("Failed to convert Document to ModelMetadata: {}", err.to_string())))?;
+            // TODO Find a better way to get the last item's _id in the list of returned
+            // documents
+            last_id = doc._id;
 
-            if let Some(last) = response.results.last() {
-                pagination_cursor = last._id.clone().and_then(|oid| Some(oid.to_string()));
-            };
+            let model = entities::model_metadata::ModelMetadata::try_from(doc)
+                .map_err(|err| ApplicationError::RepoError(err.to_string()))?;
+            
+            models.push(model);
+        }
 
-            for entry in response.results.into_iter() {
-                let model = entities::model_metadata::ModelMetadata::try_from(entry)
-                    .map_err(|err| ApplicationError::RepoError(err.to_string()))?;
-                
-                models.push(model);
-            };
+        if let Some(_id) = last_id {
+            pagination_cursor = last_id.clone().and_then(|oid| Some(oid.to_string()));
+        };
 
-            if let Some(c) = response.count {
-                count = Some(c[0].total);
-            }
+        // Return the count if requested
+        let mut count: Option<i64> = None;
+        if input.options.include_count().unwrap_or_else(|| false) {
+            count = Some(models.len() as i64);
         }
 
         return Ok(
