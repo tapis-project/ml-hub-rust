d4f63b23d480712c4b724256fa27a3e84417187c
Nathan Freeman <nathan.dean.freeman@gmail.com>
2026-04-20 16:46:13 -0500

    Fix run-all migrations script

--- deploy/k8s/minikube/migrations/run-all.sh
+++ deploy/k8s/minikube/migrations/run-all.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-kubectl apply -f ./federated-identities-imgrations-job.yml \
-    ./models-migrations-job.yml \
-    ./principals-migrations-job.yml
+kubectl apply -f ./federated-identities-migrations-job.yml \
+    -f ./models-migrations-job.yml \
+    -f ./principals-migrations-job.yml
