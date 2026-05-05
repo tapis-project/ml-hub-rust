b956df67ebaeb137b7c700f9c2597672bc453ee6
Nathan Freeman <nathan.dean.freeman@gmail.com>
2026-05-05 17:20:19 -0500

    debug actions

--- services/models/src/bin/openapi.rs
+++ services/models/src/bin/openapi.rs
@@ -1,7 +1,7 @@
 use models::presentation::http::v1::actix_web::openapi::ApiDoc;
 use utoipa::OpenApi;
 
-// Writes the OpenAPI spec for the Models API to stdout
+// Writes the OpenAPI spec for the Models API to stdout 
 pub fn main() {
     println!("{}", ApiDoc::openapi().to_json().unwrap());
 }
