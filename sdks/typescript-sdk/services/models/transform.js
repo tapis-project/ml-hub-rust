08a6c2b4489d08ce64d58d2ffe4057197f982e47
Nathan Freeman <nathan.dean.freeman@gmail.com>
2026-05-18 15:30:38 -0500

    Start deployments typescript sdk

--- sdks/typescript-sdk/services/models/transform.js
+++ sdks/typescript-sdk/services/models/transform.js
@@ -2,7 +2,7 @@ const utils = require('../../utils');
 
 console.log("MLHub Models API transform");
 try {
-    // Copy workflows spec  without transforms
+    // Copy models spec  without transforms
     utils.copy('models');
 } catch (error) {
     console.error(error);
